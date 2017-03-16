413b61ff92ae43a46bace4e6837947fc2810f48b
rrbaetge <rob.baetge@my.jcu.edu.au>
2017-03-17 09:59:57 +1000

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,10 @@
 '''Rob-roy Baetge'''
+def main():
+    name = str(input("Please enter your name"))
+    while name == "":
+        print("Invalid name")
+        name = str(input("Please enter your name"))
+    for letter in range(1, len(name), 2 ):
+        print("{}".format(name[letter]))
+
+main()
